84eae38cd8b6bc87e3aa960e38b3724d33bcbcd9
watercolorhorse <29869241+llcook@users.noreply.github.com>
2018-11-11 20:08:36 -0700

    Added pseudo-coding. Created global variables. Verified randomNum works in console, but requires refreshing the page to generate a new number to the console.

---
+++ assets/javascript/game.js
@@ -0,0 +1,45 @@
+// global variables:
+
+var wins = 0;
+var losses = 0;
+var randomNum = Math.floor(Math.random() * (120 - 19 +1)) + 19; // <--- between 19-120
+var userScore = 0;
+
+
+// wins, losses, randomNum, userScore
+// also: array of image sources (to create four diff crystals; do it in a loop; can attach img src to crystal when created and simulate having loop; var images = ["assets/crystal1", "assets/crystal2"]); loop thru array.... create four divs and set bg img or set source to be index of array)
+
+// FUNCTION 1 -- we make one so we only have one function to recall when we restart the game
+// set randomNum between 19-120 with Math.random
+// loop through images array
+// for every index, create image on screen
+// add class
+// add src
+// add crystal value as attribute
+// append image to screen
+
+
+// // example to make divs
+
+// function makeDiv() { // also add global variable for randomNum within this function, per Evan vid
+//     var array = [1, 2, 3, 4]; // can replace numbers w image sources
+//     for (var i=0; i < array.length; i++) {
+//         var div = $("<div>");
+//         div.addClass("crystal");
+//         div.attr("crystalVal", Math.floor(Math.random() * 13));
+//         $(".crystalContainer").append(div);
+//     }
+// }
+// //if userscore exceeds or is equal to, call this function again, which restarts the game
+
+// makeDiv();
+
+// // create click event on crystals (sim to calculator; create it on .crystal)
+// // use $(this)   .attr to capture value attribute; this hooks onto specific value per crystal
+// // add clicked value to userScore
+// // make conditions to check for win/loss; can do function or if/then; write if/then then abstract into a function is a good way to do it
+//     // if userScore === randomNum; ++ win/loss (can also start over upon win)
+//     // if userScore > randomNum; 
+// // recall makeDiv which will recreate page elements, reset values
+
+// //process: start with variables, then figure out what to do with them; any process you wanna do mroe than once, use function, anything you wanna use more than once, use variable
